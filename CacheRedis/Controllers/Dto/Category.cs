643efd04987ac7b7b48ba32caf8d2a52d53b9ac3
GABRIEL DA VITORIA <gabriel.santos@itixti.com.br>
2018-07-27 14:26:44 -0300

    Commit inicial projeto

---
+++ CacheRedis/Controllers/Dto/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CacheRedis.Controllers.Dto
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Category(string name)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+        }
+
+    }
+    
+}
---
+++ CacheRedis/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using CacheRedis.Controllers.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace CacheRedis.Controllers
+{
+    [Route("api/cacheredis")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IDistributedCache distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        // GET api/cacheredis/
+        [HttpGet]        
+        public ActionResult<string> Get()
+        {
+            var cacheKey = "categories";
+
+            var existingCategories = distributedCache.GetString(cacheKey);
+
+            if (!string.IsNullOrEmpty(existingCategories))
+            {
+                var retorno = JsonConvert.DeserializeObject(existingCategories);
+                return retorno.ToString();
+            }
+            else
+            {
+                var category = new Category("NOVA CATEGORIA NO REDIS");
+
+                string categoryJson = JsonConvert.SerializeObject(category);
+
+                distributedCache.SetString(cacheKey, categoryJson);
+
+                return JsonConvert.SerializeObject(category); 
+
+            }
+        }
+
+        
+    }
+}
